309e3f4727933483822a927f8dca2a3a07da71a0
nihil21 <mattia.orlandi21@gmail.com>
2021-01-18 15:52:54 +0100

    Epsilon in log to avoid NaN (bidaf_loss.py)

--- qa-nlp/utils/bidaf_loss.py
+++ qa-nlp/utils/bidaf_loss.py
@@ -7,7 +7,8 @@ def bidaf_loss(p_start: torch.FloatTensor, p_end: torch.FloatTensor,
     # Retrieve the probability of the correct start and end indexes
     p_true_start = torch.gather(p_start, 1, y_start.unsqueeze(1))  # equivalent to p_start[y_start]
     p_true_end = torch.gather(p_end, 1, y_end.unsqueeze(1))  # equivalent to p_end[y_end]
-    # Sum logarithms of start and end probabilities
-    sum_vec = torch.log(p_true_start) + torch.log(p_true_end)
+    # Sum logarithms of start and end probabilities (adding eps to avoid NaN log)
+    eps = 1e-7
+    sum_vec = torch.log(torch.add(p_true_start, eps)) + torch.log(torch.add(p_true_end, eps))
     # Sum over batch dimension
     return - sum_vec.sum() / bs
